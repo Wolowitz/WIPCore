f338b110612b21043db0cd102666c55ca9f21109
SimonDMII <simonlathuile@hotmail.it>
2011-01-02 00:15:26 +0100

    [World] Add some companion drops (sql by Kaila).

---
+++ sql/bloodycore/database/10937_companion_droprate.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO `item_loot_template` (`entry`,`item`,`ChanceOrQuestChance`) VALUES
+('740','8498','0.3'), -- Tiny Emerald Whelpling da Adolescent Whelp
+('741','8498','0.2'), -- Tiny Emerald Whelpling da Dreaming whelp
+('39384','8498','0.1'), -- Tiny Emerald Whelpling da Noxious whelp
+('20197','29960','0.1'); -- Captured Firefly da Bogflare Needler
