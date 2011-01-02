d45b253b109c59ac9d73dc49330fcff92bd8d677
SimonDMII <simonlathuile@hotmail.it>
2011-01-02 16:36:45 +0100

    [Quest/Shaman] Fix Call of Water alliance quest.

---
+++ sql/bloodycore/database/10938_call_of_water.sql
@@ -0,0 +1,2 @@
+DELETE FROM `gameobject` WHERE `id` = '202275';
+INSERT INTO `gameobject` (`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`animprogress`,`state`) VALUE ('202275','530','1','1','-2184.36','-12343.3','56.2689','3.05885','300','0','1');
