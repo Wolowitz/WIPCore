b1048d5d92daea2216fa118aa45d344d18110c42
PrinceCreed <princecreed@gmail.com>
2011-01-04 02:46:11 +0100

    [Instace/Hero] Fix Bosses immunities

---
+++ sql/bloodycore/database/10940_hero_boss_immunities.sql
@@ -0,0 +1,29 @@
+-- The Nexus
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (30510, 30529, 30532, 30540, 30398, 30397);
+
+-- Utgarde Keep
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (30748, 31679, 31656, 31673, 31674);
+
+-- Utgarde Pinnacle
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (30810, 30774, 30807, 30788);
+
+-- Stratholme Past
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31211, 31212, 31215, 31217);
+
+-- Halls of Stone
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31384, 31381, 31386);
+
+-- Halls of Lightning
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31533, 31536, 31537, 31538);
+
+-- Drak'Tharon Keep
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31362, 31350, 31349, 31360);
+
+-- Gundrak
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31370, 31365, 30530, 31368, 29932);
+
+-- Azjol-Nerub
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31612, 31611, 31610);
+
+-- The Violet Hold
+UPDATE `creature_template` SET `mechanic_immune_mask` = 617299807 WHERE `entry` IN (31507, 31510, 31508, 31511, 31509, 31512, 31506);
