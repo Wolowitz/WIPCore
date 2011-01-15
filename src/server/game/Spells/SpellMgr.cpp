2152763ff47feb8556c5009f2096c9fada954051
PrinceCreed <princecreed@gmail.com>
2011-01-15 19:02:25 +0100

    [Warrior] Shockwave is a negative effect. Closes #52

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -769,6 +769,11 @@ bool SpellMgr::_isPositiveEffect(uint32 spellId, uint32 effIndex, bool deep) con
             if (spellproto->SpellFamilyFlags[0] == 0x00002000)
                 return true;
             break;
+        case SPELLFAMILY_WARRIOR:
+            // Shockwave
+            if (spellId == 46968)
+                return false;
+            break;
         case SPELLFAMILY_PRIEST:
             switch (spellId)
             {
