3be781622866243e5aab54f514eaa5d0f8a71eba
dimiandre <dimiandre@gmail.com>
2011-01-17 23:22:26 +0100

    Add  [Helm of the Brooding Dragon] in alliance emblem of trumph vendor

---
+++ sql/bloodycore/database/11089_ally_triumph_vendor_item.sql
@@ -0,0 +1,2 @@
+-- Add another missing item in alliance emblem of trumph vendor
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (35494, 0, 47685, 0, 0, 2708);
