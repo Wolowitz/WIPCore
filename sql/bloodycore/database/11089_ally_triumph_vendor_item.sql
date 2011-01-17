a0a177110728932e3e424670061e703e33a58e04
dimiandre <dimiandre@gmail.com>
2011-01-18 00:13:52 +0100

    Add two items in alliance triumph vendor, i had forgotten them

--- sql/bloodycore/database/11089_ally_triumph_vendor_item.sql
+++ sql/bloodycore/database/11089_ally_triumph_vendor_item.sql
@@ -1,2 +1,6 @@
 -- Add another missing item in alliance emblem of trumph vendor
-INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES (35494, 0, 47685, 0, 0, 2708);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES 
+(35494, 0, 47685, 0, 0, 2708),
+(35494, 0, 47689, 0, 0, 2708),
+(35494, 0, 47693, 0, 0, 2708);
+
