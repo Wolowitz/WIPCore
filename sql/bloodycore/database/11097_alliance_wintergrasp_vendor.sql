94b97bf465e0ae1b9fd24ec9cfeb70ec792e3b6a
dimiandre <dimiandre@gmail.com>
2011-01-19 22:46:22 +0100

    [Spawn BW]Add Wintergrasp alliance vendor at Valliance Landing Camp. Thanks Liuboille for query

---
+++ sql/bloodycore/database/11097_alliance_wintergrasp_vendor.sql
@@ -0,0 +1,3 @@
+-- Add wintergrasp alliance vendor at Valliance Landing Camp
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
+(32294, 571, 1, 1, 0, 0, 5095.39, 2166.25, 365.625, 1.48167, 300, 0, 0, 12600, 0, 0, 0, 0, 0, 0);
