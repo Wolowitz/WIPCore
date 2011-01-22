09d412b1ee030734bc22a6fcf7eb41ec932bc427
dimiandre <dimiandre@gmail.com>
2011-01-22 20:49:23 +0100

    [Obsidian Sanctum]Fix Acolyte faction

---
+++ sql/bloodycore/database/11109_os_acolyte_faction.sql
@@ -0,0 +1,3 @@
+-- Acolyte faction
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`IN(31219, 31218); -- 10 man
+UPDATE `creature_template` SET `faction_A`=16, `faction_H`=16 WHERE `entry`IN(31543, 31541); -- 25 man
