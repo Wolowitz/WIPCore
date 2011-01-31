b58d897f5ec455e08a796d1e647860663436c205
SimonDMII <simonlathuile@hotmail.it>
2011-01-31 09:27:15 +0100

    Typo in previous commit

--- sql/bloodycore/database/11242_satchel_of_spoils.sql
+++ sql/bloodycore/database/11242_satchel_of_spoils.sql
@@ -1,2 +1,2 @@
--- Satchel of Spoils should drop Emblems of Conquest
-UPDATE `item_loot_template` SET `item` = '45624' WHERE `entry` = '43347';
+-- (Large) Satchel of Spoils should drop Emblems of Conquest
+UPDATE `item_loot_template` SET `item` = '45624' WHERE `entry` IN ('43347', '43346');
