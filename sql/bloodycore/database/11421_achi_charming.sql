0a34607057c25cbd496ef8e13acd86d8b7982ba6
SimonDMII <simonlathuile@hotmail.it>
2011-02-11 19:42:06 +0100

    [DB/Event] Fix criteria data of Charming achievemnt.

---
+++ sql/bloodycore/database/11421_achi_charming.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN ('6351', '6365');
+INSERT INTO `achievement_criteria_data` VALUES
+('6351', '5', '69531', '0', ''),
+('6365', '5', '69531', '0', '');
