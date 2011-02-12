c98872a61d676768dbde578467cd7e297d9eff55
dimiandre <dimiandre@gmail.com>
2011-02-12 23:17:38 +0100

    Typo

--- sql/bloodycore/database/11441_lovely_creature_flags.sql
+++ sql/bloodycore/database/11441_lovely_creature_flags.sql
@@ -1,2 +1,2 @@
 -- [Event] Fix some creature flags
-UPDATE `creature_template` SET `npcflag`=`npcflag`|1 WHERE `entry` IN (2784, 29611, 38043, 38066, 37172);
+UPDATE `creature_template` SET `npcflag`=`npcflag`|1 WHERE `entry` IN (2784, 38043, 38066, 37172);
