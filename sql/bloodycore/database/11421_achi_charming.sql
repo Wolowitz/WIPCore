58a3d39150cdf7a12b04e9c3620b77576b7b160c
SimonDMII <simonlathuile@hotmail.it>
2011-02-15 01:18:41 +0100

    [Event] Fix Charming achievement.

--- sql/bloodycore/database/11421_achi_charming.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN ('6351', '6365');
-INSERT INTO `achievement_criteria_data` VALUES
-('6351', '5', '69531', '0', ''),
-('6365', '5', '69531', '0', '');
---
+++ sql/bloodycore/database/11464_achi_charming.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN ('6351', '6365', '12846');
+INSERT INTO `achievement_criteria_data` VALUES
+('12846', '0', '0', '0', '');
